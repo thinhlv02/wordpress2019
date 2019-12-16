3fee82494f1d5ef9bc378c9f16602763188e746e
thinhlv <thinhluuvan@admicro.vn>
2019-12-16 17:02:46 +0700

    update from vccc

--- wp-content/themes/lotus/index.php
+++ wp-content/themes/lotus/index.php
@@ -1,25 +1,35 @@
 <?php
 //echo 'hello index';
 //bloginfo('description');
+
 get_header(); ?>
 
+    <div class="content">
+
+        <div id="main-content">
+
+            <?php if (have_posts()) : while (have_posts()): the_post(); ?>
+
+                <?php get_template_part('content', get_post_format()); ?>
+
+            <?php endwhile; ?>
+
+                <!--//pagination-->
+                <?php lotus_pagination(); ?>
+
+            <?php else: ?>;
+
+                <?php get_template_part('content', 'none'); ?>
+
+            <?php endif; ?>
+        </div>
+
+        <div id="sidebar">
+            <?php get_sidebar(); ?>
+        </div>
+
+    </div>
+
 <?php
-if (have_posts()) : while (have_posts()): the_post(); ?>
 
-    <!--    <h1>--><?php //the_title(); ?><!--</h1>-->
-
-    <?php get_template_part('content', get_post_format()); ?>
-
-<?php endwhile; ?>
-
-    <!--//pagination-->
-    <?php lotus_pagination(); ?>
-
-<?php else: ?>;
-
-    <?php get_template_part('content', 'none'); ?>
-
-<?php endif; ?>
-
-<?php
 get_footer();
