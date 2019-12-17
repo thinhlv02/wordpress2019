a13f279c2d845efcf58436aa18e9ad430c7f8afa
thinhlv <thinhluuvan@admicro.vn>
2019-12-17 17:00:53 +0700

    update from vccc

--- wp-content/themes/lotus/author-bio.php
+++ wp-content/themes/lotus/author-bio.php
@@ -5,13 +5,10 @@
         </div>
         <h3>
             <?php
-            printf('Written by <a href="%1$s">%2$s</a>',
-                get_author_posts_url(get_the_author_meta('ID')),
-                get_the_author());
+            printf('Written by <a href="%1$s">%2$s</a>', get_author_posts_url(get_the_author_meta('ID')), get_the_author());
             ?>
         </h3>
-        <p>
-            <?php echo get_the_author_meta('description'); ?>
-        </p>
+        <h4><?php echo 'description: ' . get_the_author_meta('description'); ?></h4>
+        <h5><?php echo 'user_email: ' . get_the_author_meta('user_email'); ?></h5>
     </div>
 </div>
