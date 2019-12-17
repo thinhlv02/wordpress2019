50a7787cda7cb413460106bb15952221305275a0
thinhlv <thinhluuvan@admicro.vn>
2019-12-17 14:40:23 +0700

    update from vccc

---
+++ wp-content/themes/lotus/content-link.php
@@ -0,0 +1,16 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <div class="entry-thumbnail">
+        <?php lotus_thumbnail('thumbnail'); ?>
+        <!--        --><?php //lotus_thumbnail('large'); ?>
+    </div>
+
+    <div class="entry-header">
+
+        <?php lotus_entry_meta(); ?>
+    </div>
+
+    <div class="entry-content">
+        <?php lotus_entry_content(); ?>
+        <?php is_single() ? lotus_entry_tag() : ''; ?>
+    </div>
+</article>
