c2bd87684f2a4dca555587e62ede577dee1eeed5
thinhlv <thinhluuvan@admicro.vn>
2019-12-17 09:34:23 +0700

    update from vccc

--- wp-content/themes/lotus/content.php
+++ wp-content/themes/lotus/content.php
@@ -1,16 +1,22 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
     <div class="entry-thumbnail">
+
         <?php lotus_thumbnail('thumbnail'); ?>
+
     </div>
+
     <div class="entry-header">
 
         <?php lotus_entry_header(); ?>
         <?php lotus_entry_meta(); ?>
 
     </div>
+
     <div class="entry-content">
 
         <?php lotus_entry_content(); ?>
 
     </div>
+
 </article>
