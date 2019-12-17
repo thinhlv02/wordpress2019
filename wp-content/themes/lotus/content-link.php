d8cf40c86069e3630aa109f193e5a867c6d70608
thinhlv <thinhluuvan@admicro.vn>
2019-12-17 15:12:38 +0700

    update from vccc

--- wp-content/themes/lotus/content-link.php
+++ wp-content/themes/lotus/content-link.php
@@ -1,16 +1,36 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <div class="entry-thumbnail">
         <?php lotus_thumbnail('thumbnail'); ?>
-        <!--        --><?php //lotus_thumbnail('large'); ?>
     </div>
 
     <div class="entry-header">
+        <?php
+        /*
+        * Lưu custom field vào biến
+        */
+        $link = get_post_meta($post->ID, 'format_link_url', true);
+        $link_description = get_post_meta($post->ID, 'format_link_description', true);
 
-        <?php lotus_entry_meta(); ?>
+        /*
+         * Hiển thị tiêu đề có link trỏ tới link gắn trong custom field
+         */
+        if (is_single())
+        {
+            printf('<h1 class="entry-title"><a href="%1$s" target="blank">%2$s</a></h1>', $link, get_the_title());
+        }
+        else
+        {
+            printf('<h2 class="entry-title"><a href="%1$s" target="blank">%2$s</a></h2>', $link, get_the_title());
+        }
+
+        ?>
     </div>
 
     <div class="entry-content">
-        <?php lotus_entry_content(); ?>
+        <?php
+        printf('<a href="%1$s" target="blank">%2$s</a>', $link, $link_description);
+        ?>
         <?php is_single() ? lotus_entry_tag() : ''; ?>
     </div>
+
 </article>
