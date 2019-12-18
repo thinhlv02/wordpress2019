6d88fc91b0a8f382d2d8812156376ce9f72c0da3
thinhlv <thinhluuvan@admicro.vn>
2019-12-18 14:43:17 +0700

    update from vccc

--- wp-content/themes/lotus/templates/contact.php
+++ wp-content/themes/lotus/templates/contact.php
@@ -2,9 +2,41 @@
 /*
  Template Name: Contact
 */
-echo 'hello contact 1121212';
+echo '<h1>hello Main Contact custom page</h1>';
+//
+//if (have_posts()): while (have_posts()) : the_post();
+//    the_content();
+//endwhile;
+//endif;
 
-if (have_posts()): while (have_posts()) : the_post();
-    the_content();
-endwhile;
-endif;
+//start page
+get_header(); ?>
+
+    <div class="content">
+
+        <div id="main-content">
+
+            <div class="contact-info">
+                <h4>Địa chỉ liên lạc</h4>
+                <p>85 vũ trọng phung, Quận Thanh Xuân, TP Hà Nội</p>
+                <p>0988 282986</p>
+            </div>
+
+            <div class="contact-form">
+                <?php echo do_shortcode('[contact-form-7 id="1916" title="Contact form 1"]'); ?>
+            </div>
+
+        </div>
+
+        <div id="sidebar">
+
+            <?php get_sidebar(); ?>
+
+        </div>
+
+    </div>
+
+<?php
+
+get_footer();
+
