25746222b725b40a5743babbf4c48d6d03e91a62
thinhlv <thinhluuvan@admicro.vn>
2019-12-18 15:56:02 +0700

    update from vccc

--- wp-content/themes/lotus/functions.php
+++ wp-content/themes/lotus/functions.php
@@ -102,11 +102,11 @@ if (!function_exists('lotus_header'))
             <?php
             if (is_home())
             {
-                printf('<h1><a href="%1$s", title="%2$s">%3$s</a></h1>', get_bloginfo('url'), get_bloginfo('description'), get_bloginfo('url'));
+                printf('<h1><a href="%1$s", title="%2$s">%3$s</a></h1>', get_bloginfo('url'), get_bloginfo('sitename'), get_bloginfo('sitename'));
             }
             else
             {
-                printf('<p><a href="%1$s", title="%2$s">%3$s</a></p>', get_bloginfo('url'), get_bloginfo('description'), get_bloginfo('url'));
+                printf('<p><a href="%1$s", title="%2$s">%3$s</a></p>', get_bloginfo('url'), get_bloginfo('description'), get_bloginfo('sitename12'));
             }
             ?>
         </div>
