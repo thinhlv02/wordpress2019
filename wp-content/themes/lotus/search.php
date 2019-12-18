a06a1844f55cef56d5b63e588cceafe328010398
thinhlv <thinhluuvan@admicro.vn>
2019-12-18 11:58:29 +0700

    update from vccc

--- wp-content/themes/lotus/search.php
+++ wp-content/themes/lotus/search.php
@@ -1,1 +1,42 @@
 <?php
+echo '<h1>hello main Search.php </h1>';
+//bloginfo('description');
+
+get_header(); ?>
+
+    <div class="content">
+
+        <div id="main-content">
+
+            <div class="search-info">
+
+            </div>
+
+            <?php if (have_posts()) : while (have_posts()): the_post(); ?>
+
+                <?php get_template_part('content', get_post_format()); ?>
+
+            <?php endwhile; ?>
+
+                <!--//pagination-->
+                <?php lotus_pagination(); ?>
+
+            <?php else: ?>;
+
+                <?php get_template_part('content', 'none'); ?>
+
+            <?php endif; ?>
+
+        </div>
+
+        <div id="sidebar">
+
+            <?php get_sidebar(); ?>
+
+        </div>
+
+    </div>
+
+<?php
+
+get_footer();
