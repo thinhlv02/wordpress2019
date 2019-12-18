efef7d978de87df90f373bf8da6a95e21f3942ed
thinhlv <thinhluuvan@admicro.vn>
2019-12-18 12:13:04 +0700

    update from vccc

--- wp-content/themes/lotus/search.php
+++ wp-content/themes/lotus/search.php
@@ -9,6 +9,15 @@ get_header(); ?>
         <div id="main-content">
 
             <div class="search-info">
+                <?php
+                $search_query = new WP_Query('s=' . $s . '&shoposts=-1');
+                $search_keyword = wp_specialchars($s, 1);
+                $search_count = $search_query->post_count;
+                //                 var_dump( $search_query );
+                //                echo '<pre>',print_r($search_query,1),'</pre>';
+                printf(__('Search results for <strong>%1$s</strong>. We found <strong>%2$s</strong> articles for you.', 'lotus'),
+                    $search_keyword, $search_count);
+                ?>
 
             </div>
 
