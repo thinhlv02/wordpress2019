21860bbc131e9b157315002c0d27d6a549483ff3
thinhlv <thinhluuvan@admicro.vn>
2019-12-18 10:06:39 +0700

    update from vccc

--- wp-content/themes/lotus/page.php
+++ wp-content/themes/lotus/page.php
@@ -14,9 +14,6 @@ get_header(); ?>
 
             <?php endwhile; ?>
 
-                <!--//pagination-->
-                <?php lotus_pagination(); ?>
-
             <?php else: ?>;
 
                 <?php get_template_part('content', 'none'); ?>
