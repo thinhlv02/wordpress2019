1366e785a36f91cf5032174e7a185f4ebb4b48d7
thinhlv <thinhluuvan@admicro.vn>
2019-12-18 12:22:20 +0700

    update from vccc

--- wp-content/themes/lotus/404.php
+++ wp-content/themes/lotus/404.php
@@ -1,1 +1,37 @@
 <?php
+//echo 'hello index';
+//bloginfo('description');
+
+get_header(); ?>
+
+    <div class="content">
+
+        <div id="main-content">
+            <?php
+            _e('<h2>404 NOT FOUND</h2>', 'lotus');
+            _e('<p>The article you were looking for was not found, but maybe try looking again!</p>', 'lotus');
+
+            get_search_form();
+
+            _e('<h3>Content categories</h3>', 'lotus');
+            echo '<div class="404-catlist">';
+            wp_list_categories(array('title_li' => ''));
+            echo '</div>';
+
+            _e('<h3>Tag Cloud</h3>', 'lotus');
+            wp_tag_cloud();
+            ?>
+
+        </div>
+
+        <div id="sidebar">
+
+            <?php get_sidebar(); ?>
+
+        </div>
+
+    </div>
+
+<?php
+
+get_footer();
