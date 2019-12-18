a03ff060af3e2ba65acb2f7cf6df1afee057e8bc
thinhlv <thinhluuvan@admicro.vn>
2019-12-18 14:49:29 +0700

    update from vccc

--- wp-content/themes/lotus/templates/full-width.php
+++ wp-content/themes/lotus/templates/full-width.php
@@ -1,1 +1,33 @@
 <?php
+/*
+Template Name: Full Width
+*/
+
+echo '<h1>hello Main FUll page custom page by thinhlv</h1>';
+
+//start page
+get_header(); ?>
+
+    <div class="content">
+
+        <div id="main-content" class="full-width">
+
+            <?php if (have_posts()) : while (have_posts()): the_post(); ?>
+
+                <?php get_template_part('content', get_post_format()); ?>
+
+            <?php endwhile; ?>
+
+            <?php else: ?>;
+
+                <?php get_template_part('content', 'none'); ?>
+
+            <?php endif; ?>
+
+        </div>
+
+    </div>
+
+<?php
+
+get_footer();
