de20590f55c887a4e7f80c0d5e127574c60fc4b8
thinhlv <thinhluuvan@admicro.vn>
2019-12-20 15:14:34 +0700

    update from vccc

--- wp-content/themes/lotus/core/plugins.php
+++ wp-content/themes/lotus/core/plugins.php
@@ -1,3 +1,28 @@
 <?php
-//echo 'i am plugins';
+function lotus_plugin_activation()
+{
+
+    // Khai bao plugin can cai dat
+    $plugins = array(
+        array(
+            'name' => 'Redux Framework',
+            'slug' => 'redux-framework',
+            'required' => true
+        )
+    );
+
+    // Thiet lap TGM
+    $config = array(
+        'menu' => 'tp_plugin_install',
+        'has_notice' => true,
+        'dismissable' => false,
+        'is_automatic' => true
+    );
+    tgmpa($plugins, $config);
+
+}
+
+add_action('tgmpa_register', 'lotus_plugin_activation');
+
+
 
