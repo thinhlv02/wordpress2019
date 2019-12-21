12183b1d33b86648b1830f1c530d437135f281cd
thinhlv <thinhluuvan@admicro.vn>
2019-12-21 10:59:22 +0700

    update from vccc

--- wp-content/themes/lotus/core/plugins.php
+++ wp-content/themes/lotus/core/plugins.php
@@ -2,7 +2,6 @@
 function lotus_plugin_activation()
 {
 
-    // Khai bao plugin can cai dat
     $plugins = array(
         array(
             'name' => 'Redux Framework',
@@ -11,7 +10,6 @@ function lotus_plugin_activation()
         )
     );
 
-    // Thiet lap TGM
     $config = array(
         'menu' => 'tp_plugin_install',//http://wordpress2019.dev.com/wp-admin/themes.php?page=tp_plugin_install&plugin_status=install
         'has_notice' => true,
