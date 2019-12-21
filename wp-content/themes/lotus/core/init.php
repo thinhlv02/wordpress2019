6b44b9c27d86e229e518b3b8b204cb07222e42c4
thinhlv <thinhluuvan@admicro.vn>
2019-12-21 11:25:19 +0700

    update from vccc

--- wp-content/themes/lotus/core/init.php
+++ wp-content/themes/lotus/core/init.php
@@ -2,4 +2,4 @@
 /* Plugin Activation */
 require_once dirname(__FILE__) . '/class-tgm-plugin-activation.php';
 require_once dirname(__FILE__) . '/plugins.php';
-require_once dirname(__FILE__) . '/options.php';
+require_once dirname(__FILE__) . '/lotus-config.php';
